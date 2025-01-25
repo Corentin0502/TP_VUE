cba8bc56f0cc15995ece5510e884a3b02fcd6d7d
Corentin0502 <corentindelot05@gmail.com>
2025-01-25 21:23:48 +0100

    commit global

---
+++ vue_TP/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      superheros: [], // Liste des superhéros
+    };
+  },
+  mounted() {
+    axios
+      .get("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
+      .then((response) => {
+        this.superheros = response.data; // Mise à jour des superhéros
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des données :", error);
+      });
+  },
+};
+</script>
